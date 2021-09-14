82502c2410273973e6aa2714626175349ef05bcb
Jaqueline Fogaça <83592978+Parkjaque@users.noreply.github.com>
2021-09-14 14:18:23 -0300

    Adicionar arquivos de projeto.

---
+++ Circulo/Class1.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+class program
+{
+    static double Pi = 3.14;
+    static void Main(string[]args)
+    {
+        //Instanciando a classe calculadora
+        Calculadora calc = new Calculadora();
+
+        //Decretando as variavéis  que o código está pedindo
+;       Console.WriteLine("Entre com valor do raio: ");
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double circ = calc.Circunferencia(raio);
+        double volume = calc.Volume(raio);
+
+        //Input do usuário 
+        Console.WriteLine("Circuferência:" + circ.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Volume:" + volume.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Valor do Pi:" + Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+}
---
+++ Circulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Calculadora
+    {
+        //Variável do PI
+        public double Pi = 3.14;
+
+        //Método que retorna o valor da circuferência 
+        
+        public double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+   
+
+    //Método que retorna o valor do volume
+
+    public double Volume(double r)
+    {
+            return 4.0 / 3.0 * Pi * r * r * r;
+    }
+
+     
+    }
+}
